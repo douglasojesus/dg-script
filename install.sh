471b41d6a68155d880f52f1ff1a2aa269675fb3a
Douglas Jesus <dougecomp@gmail.com>
2023-11-25 23:07:03 -0300

    :bulb: testando para users no root

--- install.sh
+++ install.sh
@@ -6,11 +6,14 @@ instalar_script() {
     local script_dir=$(dirname "$0")
 
     # Adicionar o diretório ao PATH
-    echo "export PATH=\$PATH:$script_dir/src" >> ~/.bashrc
-    source ~/.bashrc
+    #echo "export PATH=\$PATH:$script_dir/src" >> ~/.bashrc
+    export PATH=$PATH:$script_dir/src
+    #source ~/.bashrc
 
     # Criar um link simbólico em /usr/local/bin/
-    ln -s "$script_dir/src/dg.py" /usr/local/bin/dg
+    #ln -s "$script_dir/src/dg.py" /usr/local/bin/dg
+    rm ~/.local/bin/dg
+    ln -s "$script_dir/src/dg.py" ~/.local/bin/dg
 
     # Dar permissão de execução ao script Python
     chmod +x "$script_dir/src/dg.py"
