326a0563c1fdfdd7a5fbebd381e269c5c6f11d99
Douglas Jesus <dougecomp@gmail.com>
2023-11-25 22:22:34 -0300

    repositório atual é adicionado ao PATH

--- README.md
+++ README.md
@@ -2,8 +2,15 @@ Para rodar:
 
 clone este repositório
 
+git clone https://github.com/douglasojesus/dg-script
+
 torne o script "install.sh" executável
+
+cd dg-script
 chmod +x install.sh
 
 execute o script dentro do diretório
+
 source install.sh
+
+
--- install.sh
+++ install.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# Solicitar ao usuário instalar dependências manualmente antes de executar o script
-echo "Certifique-se de instalar as dependências manualmente antes de executar o script:"
-echo "sudo apt-get install xdotool wmctrl"
-read -p "Pressione Enter para continuar..."
-
-# Função para mover o diretório
-mover_diretorio() {
-    local origem="$1"
-    local destino="$2"
-
-    mv "$origem" "$destino"
+# Instalação de dependências
+instalar_dependencias() {
+    sudo apt-get install xdotool wmctrl
 }
 
 # Função principal
@@ -18,29 +10,20 @@ instalar_script() {
     # Obtém o diretório atual do script
     local script_dir=$(dirname "$0")
 
-    # Solicitar ao usuário o nome do usuário
-    echo "Digite seu nome de usuário: "
-    read nome_do_usuario
-
-    # Definir a pasta home e o caminho do diretório do script
-    local pasta_home="/home/$nome_do_usuario"
-    local novo_caminho="$pasta_home/$script_dir/src"
-
-    # Mover o diretório atual para o novo caminho
-    mover_diretorio "$script_dir" "$pasta_home"
-
     # Adicionar o diretório ao PATH
-    echo "export PATH=\$PATH:$novo_caminho/src" >> ~/.bashrc
+    echo "export PATH=\$PATH:$script_dir/src" >> ~/.bashrc
     source ~/.bashrc
 
     # Criar um link simbólico em /usr/local/bin/
-    ln -s "$novo_caminho/src/dg.py" /usr/local/bin/dg
+    ln -s "$script_dir/src/dg.py" /usr/local/bin/dg
 
     # Dar permissão de execução ao script Python
-    chmod +x "$novo_caminho/src/dg.py"
+    chmod +x "$script_dir/src/dg.py"
 
     echo "Instalação concluída com sucesso!"
 }
 
+# Executar instalação de dependências
+instalar_dependencias
 # Executar a função principal
 instalar_script
